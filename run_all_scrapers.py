e56f1f0f76b60f73dfdd955b29f1489b8416345d
xne0nlight <jeff@jeffjohnsononline.com>
2025-05-14 07:48:58 -0500

    Add files via upload

--- run_all_scrapers.py
+++ run_all_scrapers.py
@@ -5,3 +5,6 @@ subprocess.run(["python", "scraper_weds-mixers.py"], check=True)
 
 # Run Roto Rooters Trios
 subprocess.run(["python", "scraper_roto-rooters-trios.py"], check=True)
+
+# Run MAG 7 High Performance
+subprocess.run(["python", "scraper_mag-7-high-performance.py"], check=True)
