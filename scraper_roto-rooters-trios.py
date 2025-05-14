4ec409af8c79d1244ccaf0deee4af887e37f8788
xne0nlight <jeff@jeffjohnsononline.com>
2025-05-14 08:53:54 -0500

    Update scraper_roto-rooters-trios.py

--- scraper_roto-rooters-trios.py
+++ scraper_roto-rooters-trios.py
@@ -39,7 +39,7 @@ def download_latest_from_ftp():
     try:
         with FTP(FTP_HOST) as ftp:
             ftp.login(FTP_USERNAME, FTP_PASSWORD)
-            ftp.cwd('league_pdfs/weds-mixers')
+            ftp.cwd('league_pdfs/roto-rooters-trios')
             local_path = os.path.join(DOWNLOAD_DIR, "latest_from_ftp.pdf")
             with open(local_path, 'wb') as f:
                 ftp.retrbinary('RETR latest.pdf', f.write)
