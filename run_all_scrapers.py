fc792b9f07db4e5f3a20019b7f46f17d308e3782
xne0nlight <jeff@jeffjohnsononline.com>
2025-09-09 12:09:51 -0500

    Update run_all_scrapers.py

--- run_all_scrapers.py
+++ run_all_scrapers.py
@@ -1,11 +1,12 @@
 import subprocess
 
 # Run Weds Mixers
-#subprocess.run(["python", "scraper_weds-mixers.py"], check=True)
+subprocess.run(["python", "scraper_weds-mixers.py"], check=True)
 
 # Run Roto Rooters Trios
-#subprocess.run(["python", "scraper_roto-rooters-trios.py"], check=True)
+subprocess.run(["python", "scraper_roto-rooters-trios.py"], check=True)
 
 # Run MAG 7 High Performance
 subprocess.run(["python", "scraper_mag-7-high-performance.py"], check=True)
+
 
